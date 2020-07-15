e8f35ee98756b47f6d3b9e54e21ff21cab58c75e
kashish88 <kashish1449.cse18@chitkara.edu.in>
2020-07-15 22:55:03 +0530

    sort

---
+++ WithoutBubbleSort.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class WithoutBubbleSort {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+    a[i]=s.nextInt();
+}
+Arrays.sort(a);
+for(int i=0;i<n;i++)
+{
+System.out.println(a[i]);
+}
+
+s.close();
+    }
+}
