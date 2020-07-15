4b5ddf2ccd2faa2cdaa7ff200dc10d242add8a9c
kashish88 <kashish1449.cse18@chitkara.edu.in>
2020-07-15 22:56:41 +0530

    max

---
+++ MaxElement.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class MaxElement {
+   public static void main(String[] args) {
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    int a[]=new int[n];
+    for(int i=0;i<n;i++)
+    {
+        a[i]=s.nextInt();
+    }
+    int max=a[0];
+    for(int i=1;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    System.out.println(max);
+
+
+
+
+
+    s.close();
+   } 
+}
